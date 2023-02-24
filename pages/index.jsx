a7b399940286447d5796ae236aa30f6a184f5856
Cassie Bonacci <nerdcassie@gmail.com>
2023-02-23 23:14:10 -0600

    accessed data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
-  return <div>Hello</div>;
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('/api/echo')
+    .then(res => res.json())
+    .then(
+      (result) =>{
+        console.log(result);
+      }
+      
+    )
+  })
+  
+  return <div>{data}</div>;
+
 }
