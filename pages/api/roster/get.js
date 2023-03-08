e5afb682c7605d8cb9e9aa902150526d683575c2
hugo <hugo@hugoware.net>
2023-03-07 23:07:35 -0600

    adding route for the roster

---
+++ pages/api/roster/get.js
@@ -0,0 +1,25 @@
+import scrapeIt from 'scrape-it';
+
+export default async function handler(req, res) {
+
+  scrapeIt('https://www.coppellathletics.net/sport/wrestling/girls/?tab=roster', {
+    roster: {
+      listItem: '#athlete-table tbody tr',
+      data: {
+        name: '[data-label=PLAYER]',
+        grade: '[data-label=GRADE]',
+        pictureUrl: {
+          selector: 'img.roster-image',
+          attr: 'src'
+        }
+      }
+    }
+  })
+  .then(({ data: { roster } }) => {
+    res.json({ success: true, roster })
+  })
+  .catch(err => {
+    res.json({ success: false })
+  });
+
+}
