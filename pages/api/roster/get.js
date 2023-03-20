4e59097d569559c02d5d4d7acf3b7af28b20f7ec
hugo <hugo@hugoware.net>
2023-03-19 22:06:31 -0500

    full roster

--- pages/api/roster/get.js
+++ pages/api/roster/get.js
@@ -1,26 +1,74 @@
 import scrapeIt from 'scrape-it';
 
-export default async function handler(req, res) {
+const RECORDS = [
 
-  scrapeIt('https://www.coppellathletics.net/sport/wrestling/girls/?tab=roster', {
-    roster: {
-      listItem: '#athlete-table tbody tr',
-      data: {
-        name: '[data-label=PLAYER]',
-        grade: '[data-label=GRADE]',
-        pictureUrl: {
-          selector: 'img.roster-image',
-          attr: 'src'
+];
+
+export default async function handler(req, res) {
+  if (RECORDS.length) {
+    return res.json({ success: true, roster: RECORDS });
+  }
+  
+  // gather places to use
+  const sources = await gatherSources();
+  if (!sources.length) {
+    return res.json({ success: false, err: 'No data was found. Missing available grade levels.'});
+  }
+
+  // gather all grades levels
+  try {
+
+    const records = await Promise.all(sources.map(source => extractRoster(source.id)));
+    for (const collection of records) {
+      RECORDS.push(...collection);
+    }
+  }
+  catch (ex) {
+    return res.json({ success: false, err: 'Unable to gather all roster information' });
+  }
+
+  // give back the result
+  res.json({ success: true, roster: RECORDS });
+  
+}
+
+async function gatherSources() {
+  return new Promise((resolve, reject) => {
+    scrapeIt('https://www.coppellathletics.net/sport/wrestling/girls/?tab=roster', {
+      grades: {
+        listItem: 'select.schedule-submenu option',
+        data: {
+          name: { },
+          id: {
+            attr: 'value'
+          }
         }
       }
-    }
+    })
+    .then(({ data: { grades } }) => resolve(grades))
+    .catch(reject);
   })
-  .then(({ data: { roster } }) => {
-    res.json({ success: true, roster })
-  })
-  .catch(err => {
-    res.json({ success: false , roster: []})
+}
+
+async function extractRoster(id) {
+  return new Promise((resolve, reject) => {
+    scrapeIt(`https://www.coppellathletics.net/sport/RefreshRosterAthleteStyle2ViewComponent/${id}?schoolYear=2022-2023`, {
+      roster: {
+        listItem: '#athlete-table tbody tr',
+        data: {
+          name: '[data-label=PLAYER]',
+          grade: '[data-label=GRADE]',
+          pictureUrl: {
+            selector: 'img.roster-image',
+            attr: 'src'
+          }
+        }
+      }
+    })
+    .then(({ data: { roster } }) => {
+      resolve(roster)
+    })
+    .catch(reject);
 
   });
-
 }
