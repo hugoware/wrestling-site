e8c25a294f8369325519f213ccdcbc987abdc166
hugo <hugo@hugoware.net>
2023-04-23 22:51:25 -0500

    add back caching

--- services/videoFeed.js
+++ services/videoFeed.js
@@ -1,34 +1,39 @@
-
 import { JSDOM } from 'jsdom';
-const DOMParser = new JSDOM().window.DOMParser;
-
 import FEEDS from '../data/match-playlists.json';
 
+// access to the DOMParser 
+const DOMParser = new JSDOM().window.DOMParser;
+
+// cache for incoming data
 const CACHE = { };
 
 export default async function getLatestVideos(id) {
-  // const cached = CACHE[id];
-  // if (cached) {
-  //   return cached;
-  // }
+  
+  // check if the data has already been loaded
+  const cached = CACHE[id];
+  if (cached) {
+    return cached;
+  }
 
+  // make sure the player has any videos
   const source = FEEDS[id]
-
   if (!source) {
     return null;
   }
 
-
+  // request the videos
   return fetch(`https://www.youtube.com/feeds/videos.xml?playlist_id=${source}`)
     .then(response => response.text())
     .then(str => new DOMParser(console.log(str)).parseFromString(str, "text/xml"))
     .then(data => {
 
+      // create the record
       const record = {
         url: `https://www.youtube.com/playlist?list=${source}`,
         recent: []
       }
 
+      // gather up all videos
       data.querySelectorAll('entry').forEach(video => {
         const title = video.querySelector('media\\:title')?.textContent;
         const thumbnail = video.querySelector('media\\:thumbnail')?.getAttribute('url');
@@ -37,10 +42,10 @@ export default async function getLatestVideos(id) {
         record.recent.push({ title, thumbnail, views, url });
       })
 
+      // cache for later
       CACHE[source] = record;
 
       return record;
-
     });
   
 }
