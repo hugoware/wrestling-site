65b6f8df3dbe78d7eecc2f4b00fe0d666eaf063e
Cassie Bonacci <nerdcassie@gmail.com>
2023-04-28 23:01:08 -0500

    Update [name].jsx

--- pages/wrestler/[name].jsx
+++ pages/wrestler/[name].jsx
@@ -13,22 +13,53 @@ export default function WrestlerPage ({ wrestler, videos }){
 
     <Layout>
 
-      <img className="w-[300px] col-span-2" src={wrestler.pictureUrl}/>
+<div className="grid grid grid-cols-2 md:grid-cols-4 gap-y-10" >
+     
+<div className="grou h-30 rounded-lg bg-red-700 text-white ">
+ 
 
-      <div>
-        <h1>{wrestler.name}</h1>
-        <h2>{wrestler.grade}</h2>
-        <p>Weight class - {wrestler.weightClass}</p>
+    <div className="bg-slate-500 text-center hover:bg-red-900 text-white font-bold w-full rounded float-right ">
+      <div class=" relative group">
+        <img className="w-[100%]" src={wrestler.pictureUrl}/>
+
+
+        <div class="opacity-100 group-hover:opacity-100 duration-300 absolute h-[100%] inset-x-0 bottom-0 flex flex-col justify-between items-start p-4 gap-4  bg-slate-500 bg-opacity-50 text-white font-semibold ">
+
+        <div>
+          <h2 className="text-xl">{wrestler.grade}</h2>
+          <p className="text-xl">Weight class - {wrestler.weightClass}</p>
+        </div>
+        
+        <div>
+          <p className="text-sm">This athlete has no Bio</p>
+          <h1 className="text-sm">Click for Match videos</h1>
+        </div>
+        
+
+        </div>
+
       </div>
+      {wrestler.name}</div> 
+    
+    </div>
+    
 
+
+
+
+      
       {/* temp */}
-      <div className="grid grid-cols-2 gap-y-10" >
+      
         {videos.recent.map(video => (
-          <a href={video.url} className="block p-5">
-            <img src={video.thumbnail} />
-            <h3>{video.title}</h3>
-            <h5>Views: {video.views}</h5>
-          </a>
+         
+<div className="rounded-lg bg-red-700 text-white p-3 m-2">
+<a href={video.url}>
+  <h3 className='text-xl' >{video.title}</h3>
+  
+  <img src={video.thumbnail} />
+  <p>Views : {video.views}</p>
+</a>
+</div>
         ))}
       </div>
 
